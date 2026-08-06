e12ac1fe407c3ea21d0d3a2aa6e271b40a617339
juancortespositiva <juan.cortes@positiva.gov.co>
2026-08-05 20:30:44 -0500

    test: validar autenticacion federada con GCP

--- provider.tf
+++ provider.tf
@@ -2,3 +2,9 @@ provider "google" {
   project = "analitica-contact-center-dev"
   region  = "us-central1"
 }
+
+data "google_project" "actual" {}
+
+output "proyecto_gcp_verificado" {
+  value = data.google_project.actual.project_id
+}
